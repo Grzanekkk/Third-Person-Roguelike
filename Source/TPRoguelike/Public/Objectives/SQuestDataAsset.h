bf4e1b21e98d4639c18b1a6ab1920ec11129bae5
Jan Puto <tofukles@gmail.com>
2023-02-04 13:53:25 +0100

    quest data asset

---
+++ Source/TPRoguelike/Public/Objectives/SQuestDataAsset.h
@@ -0,0 +1,17 @@
+// Made by Jan Puto 2023 :D
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SQuestDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPROGUELIKE_API USQuestDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
