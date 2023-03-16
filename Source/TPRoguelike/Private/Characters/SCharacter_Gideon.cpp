bb7c75e252cf3acf74f13e5fb8b5e2634753a6a6
Jan Puto <tofukles@gmail.com>
2023-03-16 21:17:48 +0100

    Adjusting gideon to new model, moving stuff in folders

---
+++ Source/TPRoguelike/Private/Characters/SCharacter_Gideon.cpp
@@ -0,0 +1,5 @@
+// Made by Jan Puto 2023 :D
+
+
+#include "Characters/SCharacter_Gideon.h"
+
---
+++ Source/TPRoguelike/Public/Characters/SCharacter_Gideon.h
@@ -0,0 +1,17 @@
+// Made by Jan Puto 2023 :D
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/SCharacter.h"
+#include "SCharacter_Gideon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPROGUELIKE_API ASCharacter_Gideon : public ASCharacter
+{
+	GENERATED_BODY()
+	
+};
